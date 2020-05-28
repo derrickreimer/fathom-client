3e8a7cc1f2a862cd53fd9e0526e6792818f47a33
Derrick Reimer <derrickreimer@gmail.com>
2020-05-28 10:05:42 -0500

    Remove the old fathom queue, implement our own

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,10 @@
 interface Fathom {
   (...args: any[]): void;
-  q?: Array<IArguments>;
 }
 
-declare global {
-  interface Window {
-    fathom: Fathom;
-  }
-}
-
-const getFathom = (): Fathom => {
-  return (window.fathom =
-    window.fathom ||
-    function() {
-      (window.fathom.q = window.fathom.q || []).push(arguments);
-    });
+export type PageViewOptions = {
+  url?: string;
+  referrer?: string;
 };
 
 // refer to https://usefathom.com/support/tracking-advanced
@@ -26,49 +16,95 @@ export type LoadOptions = {
   includedDomains?: string[];
   excludedDomains?: string[];
   spa?: 'auto' | 'history' | 'hash';
+};
+
+type FathomCommand =
+  | ['trackPageview']
+  | ['trackPageview', PageViewOptions]
+  | ['trackGoal', string, number];
+
+declare global {
+  interface Window {
+    fathom?: Fathom;
+    __fathomClientQueue: FathomCommand[];
+  }
 }
 
-export const load = (siteId: string, opts?: LoadOptions): void => {
-  window.fathom =
-    window.fathom ||
-    function() {
-      (window.fathom.q = window.fathom.q || []).push(arguments);
-    };
+/**
+ * Enqueues a command to dispatch to fathom when the library is loaded.
+ *
+ * @param command - A set of arguments to dispatch to fathom later.
+ */
+const enqueue = (command: FathomCommand): void => {
+  window.__fathomClientQueue = window.__fathomClientQueue || [];
+  window.__fathomClientQueue.push(command);
+};
 
+/**
+ * Flushes the command queue.
+ */
+const flushQueue = (): void => {
+  window.__fathomClientQueue = window.__fathomClientQueue || [];
+  window.__fathomClientQueue.forEach(command => window.fathom(...command));
+  window.__fathomClientQueue = [];
+};
+
+export const load = (siteId: string, opts?: LoadOptions): void => {
   let tracker = document.createElement('script');
   let firstScript = document.getElementsByTagName('script')[0];
 
   tracker.id = 'fathom-script';
   tracker.async = true;
   tracker.setAttribute('site', siteId);
-  tracker.src = (opts && opts.url) ? opts.url : 'https://cdn.usefathom.com/script.js';
+  tracker.src =
+    opts && opts.url ? opts.url : 'https://cdn.usefathom.com/script.js';
   if (opts) {
     if (opts.auto !== undefined) tracker.setAttribute('auto', `${opts.auto}`);
-    if (opts.honorDNT !== undefined) tracker.setAttribute('honor-dnt', `${opts.honorDNT}`);
-    if (opts.canonical !== undefined) tracker.setAttribute('canonical', `${opts.canonical}`);
-    if (opts.includedDomains) tracker.setAttribute('included-domains', opts.includedDomains.join(','));
-    if (opts.excludedDomains) tracker.setAttribute('excluded-domains', opts.excludedDomains.join(','));
+    if (opts.honorDNT !== undefined)
+      tracker.setAttribute('honor-dnt', `${opts.honorDNT}`);
+    if (opts.canonical !== undefined)
+      tracker.setAttribute('canonical', `${opts.canonical}`);
+    if (opts.includedDomains)
+      tracker.setAttribute('included-domains', opts.includedDomains.join(','));
+    if (opts.excludedDomains)
+      tracker.setAttribute('excluded-domains', opts.excludedDomains.join(','));
     if (opts.spa) tracker.setAttribute('spa', opts.spa);
   }
+  tracker.onload = flushQueue;
   firstScript.parentNode.insertBefore(tracker, firstScript);
 };
 
-export const setSiteId = (siteId: string): void => {
-  let fathom = getFathom();
-  fathom('set', 'siteId', siteId);
-};
-
-export type PageViewOptions = {
-  url?: string;
-  referrer?: string;
-}
-
+/**
+ * Tracks a pageview.
+ *
+ * @param opts - An optional `url` or `referrer` to override auto-detected values.
+ */
 export const trackPageview = (opts?: PageViewOptions): void => {
-  let fathom = getFathom();
-  opts ? fathom('trackPageview', opts) : fathom('trackPageview');
+  if (window.fathom) {
+    if (opts) {
+      window.fathom('trackPageview', opts);
+    } else {
+      window.fathom('trackPageview');
+    }
+  } else {
+    if (opts) {
+      enqueue(['trackPageview', opts]);
+    } else {
+      enqueue(['trackPageview']);
+    }
+  }
 };
 
+/**
+ * Tracks a goal.
+ *
+ * @param id - The goal ID.
+ * @param cents - The value in cents.
+ */
 export const trackGoal = (id: string, cents: number) => {
-  let fathom = getFathom();
-  fathom('trackGoal', id, cents);
+  if (window.fathom) {
+    window.fathom('trackGoal', id, cents);
+  } else {
+    enqueue(['trackGoal', id, cents]);
+  }
 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,8 @@
 import * as Fathom from '../src';
 
-const args = function() {
-  return arguments;
-};
-
 beforeEach(() => {
   window.fathom = undefined;
+  delete window.__fathomClientQueue;
 });
 
 describe('load', () => {
@@ -22,7 +19,6 @@ describe('load', () => {
 
     const fathomScript = document.getElementById('fathom-script');
     expect(fathomScript.src).toBe('https://cdn.usefathom.com/script.js');
-    expect(typeof window.fathom).toBe('function');
   });
 
   it('injects the Fathom script with options', () => {
@@ -39,32 +35,29 @@ describe('load', () => {
     expect(fathomScript.getAttribute('included-domains')).toBe('bobheadxi.dev');
     expect(fathomScript.getAttribute('auto')).toBe('false');
     expect(fathomScript.getAttribute('honor-dnt')).toBe(null);
-    expect(typeof window.fathom).toBe('function');
-  });
-});
-
-describe('setSiteId', () => {
-  it('enqueues the operation if fathom is not loaded', () => {
-    Fathom.setSiteId('XXX');
-    expect(window.fathom.q).toContainEqual(args('set', 'siteId', 'XXX'));
   });
 
-  it('calls the fathom function if loaded', () => {
-    return new Promise(resolve => {
-      window.fathom = (...args) => {
-        expect(args).toStrictEqual(['set', 'siteId', 'XXX']);
-        resolve();
-      };
+  it('runs the queue after load', () => {
+    Fathom.trackPageview();
 
-      Fathom.setSiteId('XXX');
-    });
+    window.fathom = jest.fn();
+
+    const firstScript = document.createElement('script');
+    document.body.appendChild(firstScript);
+    Fathom.load();
+
+    // simulate 'onload' firing
+    const fathomScript = document.getElementById('fathom-script');
+    fathomScript.dispatchEvent(new Event('load'));
+
+    expect(window.fathom.mock.calls[0][0]).toBe('trackPageview');
   });
 });
 
 describe('trackPageview', () => {
   it('enqueues the operation if fathom is not loaded', () => {
     Fathom.trackPageview();
-    expect(window.fathom.q).toContainEqual(args('trackPageview'));
+    expect(window.__fathomClientQueue).toStrictEqual([['trackPageview']]);
   });
 
   it('calls the fathom function if loaded', () => {
@@ -96,7 +89,9 @@ describe('trackPageview', () => {
 describe('trackGoal', () => {
   it('enqueues the operation if fathom is not loaded', () => {
     Fathom.trackGoal('Sign Up', 0);
-    expect(window.fathom.q).toContainEqual(args('trackGoal', 'Sign Up', 0));
+    expect(window.__fathomClientQueue).toStrictEqual([
+      ['trackGoal', 'Sign Up', 0]
+    ]);
   });
 
   it('calls the fathom function if loaded', () => {
